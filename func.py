05927d0600c26343c498dfe87d354ac8cf99140c
widerhin <widerhin@yandex.ru>
2023-12-18 16:38:41 +0300

    Fin

---
+++ func.py
@@ -0,0 +1,59 @@
+import FileWorks
+import Notes
+import ui
+
+number = 1  # сколько знаков МИНИМУМ может быть в тексте заметки
+
+
+def add():
+    note = ui.create_note(number)
+    array = FileWorks.read_file()
+    for notes in array:
+        if Notes.Note.get_id(note) == Notes.Note.get_id(notes):
+            Notes.Note.set_id(note)
+    array.append(note)
+    FileWorks.write_file(array, 'a')
+    print('Заметка добавлена...')
+
+
+def show(text):
+    logic = True
+    array = FileWorks.read_file()
+    if text == 'date':
+        date = input('Введите дату в формате dd.mm.yyyy: ')
+    for notes in array:
+        if text == 'all':
+            logic = False
+            print(Notes.Note.map_note(notes))
+        if text == 'id':
+            logic = False
+            print('ID: ' + Notes.Note.get_id(notes))
+        if text == 'date':
+            logic = False
+            if date in Notes.Note.get_date(notes):
+                print(Notes.Note.map_note(notes))
+    if logic == True:
+        print('Нет ни одной заметки...')
+
+
+def id_edit_del_show(text):
+    id = input('Введите id необходимой заметки: ')
+    array = FileWorks.read_file()
+    logic = True
+    for notes in array:
+        if id == Notes.Note.get_id(notes):
+            logic = False
+            if text == 'edit':
+                note = ui.create_note(number)
+                Notes.Note.set_title(notes, note.get_title())
+                Notes.Note.set_body(notes, note.get_body())
+                Notes.Note.set_date(notes)
+                print('Заметка изменена...')
+            if text == 'del':
+                array.remove(notes)
+                print('Заметка удалена...')
+            if text == 'show':
+                print(Notes.Note.map_note(notes))
+    if logic == True:
+        print('Такой заметки нет, возможно, вы ввели неверный id')
+    FileWorks.write_file(array, 'a')
